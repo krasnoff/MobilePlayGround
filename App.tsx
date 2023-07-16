deca6e3d97b58fcb1ceeb802afb613e2018175fc
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-07-16 22:19:26 +0300

    start implement react navigator

--- App.tsx
+++ App.tsx
@@ -6,45 +6,19 @@
  */
 
 import React, { useState } from 'react';
-import {
-  Image,
-  ScrollView,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import HomeScreen from './pages/homescreen/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-  const [text, setText] = useState<string>('')
-
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image source={{
-          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-        }} style={{width: 200, height: 200}}></Image>
-      </View>
-      <View style={{
-        alignItems: 'center'
-        }}>
-        <TextInput style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            width: '80%'
-          }} placeholder='You can type in me'
-          onChangeText={newText => setText(newText)}
-        ></TextInput>
-      </View>
-      <View  style={{
-          width: '90%'
-        }}>
-        <Text>{text}</Text>
-      </View>
-    </ScrollView>
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home screen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ pages/homescreen/HomeScreen.tsx
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import {
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+function HomeScreen(): JSX.Element {
+  const [text, setText] = useState<string>('')
+
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }} style={{width: 200, height: 200}}></Image>
+      </View>
+      <View style={{
+        alignItems: 'center'
+        }}>
+        <TextInput style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            width: '80%'
+          }} placeholder='You can type in me'
+          onChangeText={newText => setText(newText)}
+        ></TextInput>
+      </View>
+      <View  style={{
+          width: '90%'
+        }}>
+        <Text>{text}</Text>
+      </View>
+    </ScrollView>
+    
+  );
+}
+
+export default HomeScreen;
