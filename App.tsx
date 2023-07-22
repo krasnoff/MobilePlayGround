1a5e6d54d668c501c634c14a1f48a43ea7d96f81
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-07-22 09:09:19 +0300

    custom header

--- App.tsx
+++ App.tsx
@@ -10,14 +10,51 @@ import HomeScreen from './pages/homescreen/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DetailsScreen from './pages/details-screen/DetailedScreen';
+import { useFonts } from 'expo-font';
+import { Text, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
+function LogoTitle() {
+  const [fontsLoaded] = useFonts({
+    'Heebo-Bold': require('./assets/fonts/heebo/Heebo-Bold.ttf'),
+  });
+
+  return (
+    <View>
+      <Text style={{ 
+        fontFamily: 'Heebo-Bold', 
+        fontSize: 20,
+        color: '#ffffff'
+      }}>כותרת לדוגמה</Text>
+    </View>
+  );
+}
+
 function App(): JSX.Element {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            // title: 'כותרת לדוגמה',
+            headerTitle: (() => <LogoTitle />),
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',  
+              fontFamily: 'Heebo-Bold'            
+            },
+            headerTitleAlign: "center",
+            headerShadowVisible: true, 
+          }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
