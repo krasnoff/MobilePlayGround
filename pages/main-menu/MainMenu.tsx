75112f59c7de5325afc1d6b276fb9c66b44ba450
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-07-31 22:12:12 +0300

    add new screen

---
+++ pages/main-menu/MainMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+function MainMenu(): JSX.Element {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Main Menu</Text>
+      </View>
+    );
+}
+
+export default MainMenu;
